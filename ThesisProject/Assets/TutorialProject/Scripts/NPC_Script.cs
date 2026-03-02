fdc4730e8cffa6a99198657c1b77f73c09cd9f5b
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2026-03-02 22:17:53 +0100

    update IInteractable

--- ThesisProject/Assets/TutorialProject/Scripts/NPC_Script.cs
+++ ThesisProject/Assets/TutorialProject/Scripts/NPC_Script.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC_Script : IInteractable
+public class NPC : MonoBehaviour, IInteractable
 {
     // Start is called before the first frame update
     void Start()
