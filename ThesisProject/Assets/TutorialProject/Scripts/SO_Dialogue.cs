542f79a5a946def291495d006c202385c1012f94
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2026-03-02 13:41:25 +0100

    Update SO_Dialogue.cs

--- ThesisProject/Assets/TutorialProject/Scripts/SO_Dialogue.cs
+++ ThesisProject/Assets/TutorialProject/Scripts/SO_Dialogue.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[CreateAssetMenu(fileName = "New_dialogue", menuName = "Dialogue")]
 
-public class SO_NPC : MonoBehaviour
+public class SO_NPC : ScriptableObject
 {
     // Start is called before the first frame update
     void Start()
