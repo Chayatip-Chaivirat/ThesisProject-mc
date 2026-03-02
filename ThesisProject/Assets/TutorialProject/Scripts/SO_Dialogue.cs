1dac838b309cbeb3e709ca205a030cd4d80d2807
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2026-03-02 13:54:30 +0100

    Update SO_Dialogue.cs

--- ThesisProject/Assets/TutorialProject/Scripts/SO_Dialogue.cs
+++ ThesisProject/Assets/TutorialProject/Scripts/SO_Dialogue.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class SO_NPC : ScriptableObject
 {
+    [System.Serializable] dialogueInfo;
+        public string dialogue;
     // Start is called before the first frame update
     void Start()
     {
